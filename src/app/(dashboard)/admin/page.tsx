19e18dec01eb43a4e7ff26d40013225386c05ec3
KalolwalaCodes <tech@kalolwala.com>
2024-09-08 21:24:10 +0530

    Count Chart Done

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,12 @@
 import UserCard from "@/components/UserCard"
+import CountChart from "@/components/CountChart"
 
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
 
       {/* LEFT */}
-      <div className="w-full md:w-2/3">
+      <div className="w-full md:w-2/3 flex flex-col gap-4">
 
       {/* USER CARDS */}
       <div className="flex gap-4 justify-between flex-wrap">
@@ -14,14 +15,31 @@ const AdminPage = () => {
         <UserCard type="parent" />
         <UserCard type="staff" />
       </div>
+      
+
+      {/* MIDDLE CHART */}
+      <div className="flex gap-4 flex-col lg:flex-row">
+{/* COUNT CHART */}
+<div className="w-full lg:w-1/3 h-[450px]">
+<CountChart />
+</div>
+
+{/* ATTENDANCE CHART */}
+<div className="w-full lg:w-2/3 h-[450px]"></div>
+
       </div>
+
+      {/* BOTTOM CHART */}
+      <div className=""></div>
+
+
 
        {/* RIGHT */}
        <div className="w-full md:w-1/3">R</div>
       
 
 
-
+       </div>
     </div>
   )
 }
