90936f1f653d897b5256d13af709353739612bcc
ijeetu <ijeetuvishwakarma@gmail.com>
2024-09-20 23:22:01 +0530

    Table Search Iconn & Pagination Added

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,40 @@
+import Pagination from "@/components/Pagination"
+import TableSearch from "@/components/TableSearch"
+import Image from "next/image"
+const TeacherList = () => {
+  return (
+    <div className="bg-white rounded-xl p-4 m-4 flex-1">
+      
+
+      {/* TOP */}
+      <div className="flex justify-between items-center">
+        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <div className="flex flex-col md:flex-row gap-4 items-center w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-outSky text-white" >
+              <Image src="/filter.png" alt="search" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-outSky text-white" >
+              <Image src="/sort.png" alt="search" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-outSky text-white" >
+              <Image src="/plus.png" alt="search" width={14} height={14} />
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+      {/* LIST */}
+
+
+{/* PAGINATION */}
+<Pagination />
+
+
+    </div>
+  )
+}
+
+export default TeacherList
